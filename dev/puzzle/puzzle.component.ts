d71de7b9af8034b294725079c01a23172d95e05f
SangND <dacsang97@gmail.com>
2016-04-24 16:52:22 +0700

    - Create my Puzzle Game.

---
+++ dev/puzzle/puzzle.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector : "my-puzzle",
+    template : `
+        <section> 
+            Enter your name : 
+            <input type="text">
+        </section>
+    `
+})
+
+export class Puzzle {
+
+}
