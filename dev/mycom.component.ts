850b624600586070787390a7fb596b7a30880dc1
SangND <dacsang97@gmail.com>
2016-04-24 16:19:33 +0700

    - Using External Template HTML

--- dev/mycom.component.ts
+++ dev/mycom.component.ts
@@ -1,19 +1,19 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {TestComponent} from "./test.component";
 
 @Component({
     selector : 'mycom',
-    template : `
-        My name is <span [style.color]="inputElement.value=== 'yes' ? 'red' : 'black'">{{ name }}</span> and this is my first Angular 2 App. <span [class.is-awesome]="inputElement.value === 'yes'">It is so awesome.</span>
-        <br>
-        <br>
-        Is it awesome? 
-        <input type="text" #inputElement (keyup)="0">
-        <button [disabled]="inputElement.value !== 'yes'">Only enable if 'yes' was entered</button>
-    `,
-    styleUrls : ['src/css/mycom.css']
+    templateUrl : 'template/mycom.tpl.html',
+    styleUrls : ['src/css/mycom.css'],
+    directives : [TestComponent]
 
 })
 
-export class MyComComponent {
-    name = "SangND";
+export class MyComComponent implements OnInit{
+    name : string;
+    ngOnInit():any {
+        this.name = "SangND";
+    }
+
+
 }
---
+++ dev/test.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector : 'test',
+    templateUrl : 'template/test.tpl.html'
+})
+
+export class TestComponent {
+
+}
